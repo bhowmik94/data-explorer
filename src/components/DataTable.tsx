da14317e2d8c120d5f791202324dbefe3e0fd50c
bhowmik94 <sourav.bhowmik@stud.uni-bamberg,de>
2025-12-24 23:43:22 +0100

    added CSV parser and custom pagination logic

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -2,6 +2,9 @@ import { Table } from "react-bootstrap";
 import type { NormalizedRow } from "../dtos/utils";
 import type { SortOrder, tableSort } from "../dtos/dashboard";
 import { FaCaretUp, FaCaretDown } from "react-icons/fa6";
+import { useState } from "react";
+import "../styles/dataTable.css";
+import Pagination from "react-bootstrap/Pagination";
 
 type DataTableProps = {
   rows: NormalizedRow[];
@@ -9,7 +12,7 @@ type DataTableProps = {
   sortColumn: tableSort["column"];
   sortDirection: tableSort["order"];
   onSort: (column: string, direction: SortOrder) => void;
-}
+};
 
 export default function DataTable({
   rows,
@@ -18,61 +21,151 @@ export default function DataTable({
   sortDirection,
   onSort,
 }: DataTableProps) {
-  return (
-    <Table bordered responsive>
-      <thead>
-        <tr>
-          {rows.length != 0 && <th>#</th>}
-          {columns && columns.map((col) => (
-            <th key={col}>
-              <div className="th-content">
-                <span>{col} </span>
-                <div className="sort-icons">
-                  <span
-                    onClick={() => onSort(col, "asc")}
-                    className={
-                      sortColumn === col && sortDirection === "asc"
-                        ? "sort-icon active"
-                        : "sort-icon"
-                    }
-                  >
-                    <FaCaretUp />
-                  </span>
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageSize, setPageSize] = useState(25);
 
-                  <span
-                    onClick={() => onSort(col, "desc")}
-                    className={
-                      sortColumn === col && sortDirection === "desc"
-                        ? "sort-icon active"
-                        : "sort-icon"
-                    }
-                  >
-                    <FaCaretDown />
-                  </span>
-                </div>
-              </div>
-            </th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {rows.map((row, idx) => (
-          <tr
-            key={idx}
-            className={row.missingCols.size > 0 ? "table-warning" : ""}
-          >
-            <td>{idx + 1}</td>
-            {columns && columns.map((col) => (
-              <td
-                key={col}
-                className={row.missingCols.has(col) ? "empty-cell" : ""}
-              >
-                {String(row.data[col])}
-              </td>
-            ))}
+  const startIndex = (currentPage - 1) * pageSize;
+  const endIndex = pageSize * currentPage;
+
+  const currentRows = rows.slice(startIndex, endIndex);
+  const totalPages = Math.ceil(rows.length / pageSize);
+
+  const windowSize = 2;
+  const start = Math.max(2, currentPage - windowSize);
+  const end = Math.min(totalPages - 1, currentPage + windowSize);
+
+  const handlePagination = function (direction: string) {
+    setCurrentPage((prevPage) => {
+      switch (direction) {
+        case "next":
+          return Math.min(prevPage + 1, totalPages);
+
+        case "prev":
+          return Math.max(prevPage - 1, 1);
+
+        case "last":
+          return totalPages;
+
+        case "first":
+          return 1;
+
+        default:
+          return prevPage;
+      }
+    });
+  };
+
+  return (
+    <>
+      <Table bordered responsive>
+        <thead>
+          <tr>
+            {rows.length != 0 && <th>#</th>}
+            {columns &&
+              columns.map((col) => (
+                <th key={col}>
+                  <div className="th-content">
+                    <span>{col} </span>
+                    <div className="sort-icons">
+                      <span
+                        onClick={() => onSort(col, "asc")}
+                        className={
+                          sortColumn === col && sortDirection === "asc"
+                            ? "sort-icon active"
+                            : "sort-icon"
+                        }
+                      >
+                        <FaCaretUp />
+                      </span>
+
+                      <span
+                        onClick={() => onSort(col, "desc")}
+                        className={
+                          sortColumn === col && sortDirection === "desc"
+                            ? "sort-icon active"
+                            : "sort-icon"
+                        }
+                      >
+                        <FaCaretDown />
+                      </span>
+                    </div>
+                  </div>
+                </th>
+              ))}
           </tr>
-        ))}
-      </tbody>
-    </Table>
+        </thead>
+        <tbody>
+          {currentRows.map((row, idx) => (
+            <tr
+              key={idx}
+              className={row.missingCols.size > 0 ? "table-warning" : ""}
+            >
+              <td>{idx + 1}</td>
+              {columns &&
+                columns.map((col) => (
+                  <td
+                    key={col}
+                    className={row.missingCols.has(col) ? "empty-cell" : ""}
+                  >
+                    {String(row.data[col])}
+                  </td>
+                ))}
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      <Pagination className="justify-content-center mt-3">
+        <Pagination.First
+          disabled={currentPage === 1}
+          onClick={() => handlePagination("first")}
+        />
+        <Pagination.Prev
+          disabled={currentPage === 1}
+          onClick={() => handlePagination("prev")}
+        />
+
+        <Pagination.Item
+          active={currentPage === 1}
+          onClick={() => setCurrentPage(1)}
+        >
+          1
+        </Pagination.Item>
+
+        {start > 2 && <Pagination.Ellipsis disabled />}
+
+        {Array.from({ length: end - start + 1 }, (_, i) => {
+          const page = start + i;
+          return (
+            <Pagination.Item
+              key={page}
+              active={page === currentPage}
+              onClick={() => setCurrentPage(page)}
+            >
+              {page}
+            </Pagination.Item>
+          );
+        })}
+
+        {end < totalPages - 1 && <Pagination.Ellipsis disabled />}
+
+        {totalPages > 1 && (
+          <Pagination.Item
+            active={currentPage === totalPages}
+            onClick={() => setCurrentPage(totalPages)}
+          >
+            {totalPages}
+          </Pagination.Item>
+        )}
+
+        <Pagination.Next
+          disabled={currentPage === totalPages}
+          onClick={() => handlePagination("next")}
+        />
+        <Pagination.Last
+          disabled={currentPage === totalPages}
+          onClick={() => handlePagination("last")}
+        />
+      </Pagination>
+    </>
   );
 }
--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -1,31 +1,65 @@
 import type { ChangeEvent } from "react";
 import { validateJSON } from "../utils/validation";
+import Papa from "papaparse";
 
 type FileUploadProps = {
   onDataParsed: (data: Record<string, unknown>[]) => void;
 };
 
 export default function FileUpload({ onDataParsed }: FileUploadProps) {
-  const onFileChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0];
-    if (!file) return;
-    const reader = new FileReader();
+  const reader = new FileReader();
+
+  const handleJSON = function (file: File) {
     reader.onload = () => {
       try {
         const parsedJSON = JSON.parse(reader.result as string);
         const { valid, message } = validateJSON(parsedJSON);
-        console.log(parsedJSON)
+        console.log(parsedJSON);
         if (!valid) {
           alert(message);
           return;
         }
-        onDataParsed(parsedJSON)
+        onDataParsed(parsedJSON);
       } catch {
-        alert('There have been some problem uploading the file');
+        alert("There have been some problem uploading the file");
       }
     };
 
     reader.readAsText(file);
+  };
+
+  const handleCSV = function (file: File) {
+    Papa.parse(file, {
+      header: true,
+      skipEmptyLines: true,
+      complete: (results) => {
+        const parsedData = results.data as Record<string, unknown>[];
+        console.log("Parsed CSV Data:", parsedData);
+        onDataParsed(parsedData);
+      },
+      error: (error) => {
+        console.error("Error parsing CSV:", error);
+        alert("There was a problem uploading the CSV file");
+      },
+    });
+  };
+
+  const onFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (!file) return;
+
+    const fileName = file.name.toLowerCase();
+    const fileType = file.type;
+
+    if (fileType === "application/json" || fileName.endsWith(".json"))
+      handleJSON(file);
+    else if (fileType === "text/csv" || fileName.endsWith(".csv"))
+      handleCSV(file);
+    else {
+      alert("Unsupported file type! Please upload a JSON or CSV file.");
+      // Clear the input
+      e.target.value = "";
+    }
   };
 
   return <input type="file" onChange={onFileChange} />;
